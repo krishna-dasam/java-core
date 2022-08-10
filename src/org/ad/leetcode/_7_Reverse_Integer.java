77c44b352cbde52c97b1d1139407cea85b4fc62a
Krishna Dasam <kdasam@Krishnas-MacBook-Pro-2.local>
2022-08-10 18:47:08 -0500

    Reverse Integer

---
+++ src/org/ad/leetcode/_7_Reverse_Integer.java
@@ -0,0 +1,12 @@
+package org.ad.leetcode;
+
+public class _7_Reverse_Integer {
+
+    public int reverseInteger (int num) {
+        int reverse = 0;
+        if(num!=0) {
+
+        }
+        return reverse;
+    }
+}
