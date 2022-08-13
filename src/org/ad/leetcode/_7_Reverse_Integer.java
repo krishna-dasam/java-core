371a84eb02f2946f6bb589c67e104887f323ab4a
Krishna Dasam <kdasam@Krishnas-MacBook-Pro-2.local>
2022-08-13 08:40:55 -0500

    Reverse Integer

--- src/org/ad/leetcode/_7_Reverse_Integer.java
+++ src/org/ad/leetcode/_7_Reverse_Integer.java
@@ -4,8 +4,9 @@ public class _7_Reverse_Integer {
 
     public int reverseInteger (int num) {
         int reverse = 0;
-        if(num!=0) {
-
+        while(num!=0) {
+            reverse = reverse*10 + num%10;
+            num = num/10;
         }
         return reverse;
     }
