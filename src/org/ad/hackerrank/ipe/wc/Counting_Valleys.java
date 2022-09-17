4cce9d63c7c87086340dc024813cef2ec859ec21
Krishna Dasam <kdasam@Krishnas-MBP-2.attlocal.net>
2022-09-17 09:25:42 -0500

    Counting Valley

---
+++ src/org/ad/hackerrank/ipe/wc/Counting_Valleys.java
@@ -0,0 +1,17 @@
+package org.ad.hackerrank.ipe.wc;
+
+public class Counting_Valleys {
+
+    public int count(int n, String str) {
+        int valley = 0;
+        int level = 0;
+        for (Character c : str.toCharArray()) {
+            if (c == 'U') {
+                level++;
+                if (level == 0) valley++;
+            }
+            if (c == 'D') level--;
+        }
+        return valley;
+    }
+}
