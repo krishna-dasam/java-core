cb9ce40f2939dd4b82a8b2eafe54df292cb03e0c
Krishna Dasam <kdasam@Krishnas-MacBook-Pro-2.local>
2022-09-18 15:32:10 -0500

    Jumping on Clouds

---
+++ src/org/ad/hackerrank/ipe/wc/Jumping_On_Clouds.java
@@ -0,0 +1,15 @@
+package org.ad.hackerrank.ipe.wc;
+
+public class Jumping_On_Clouds {
+
+    public int minNumJumps(int[] clouds) {
+        int jumps = 0;
+        for (int i = 0; i < clouds.length - 1; i++) {
+            if (!(i + 2 == clouds.length || clouds[i + 2] == 1)) {
+                i++;
+            }
+            jumps++;
+        }
+        return jumps;
+    }
+}
